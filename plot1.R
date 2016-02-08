1f0a34b425e4f86b3ca0d2cf989e62498c2f4fe2
tteister <tteister10@gmail.com>
2016-02-08 15:14:16 -0600

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,3 +1,4 @@
+##This creates plot 1
 plot1 <- function() {
   hist(df$Global_active_power, main = paste("Global Active Power"), col="red", xlab="Global Active Power (kilowatts)")
   dev.copy(png, file="plot1.png", width=480, height=480)
